7d60476016473eee96c009b48f215ab6ce8103ef
Ognjen <ognjen.lukajic@dci-student.org>
2024-04-09 23:54:57 +0200

    char pics improved even further

--- src/components/CharacterSelection.jsx
+++ src/components/CharacterSelection.jsx
@@ -19,19 +19,19 @@ import React, { useState } from 'react';
 import styles from '../stylz/CharacterSelection.module.css';
 
 import paulImage from '../assets/paul_atreides.png';
+import chaniImage from '../assets/chani.jpeg';
+import stilgarImage from '../assets/stilgar.jpeg';
 import jessicaImage from '../assets/lady_jessica.jpeg';
 import duncanImage from '../assets/duncan_idaho.jpeg';
 import gurneyImage from '../assets/gurney_halleck.jpeg';
-import stilgarImage from '../assets/stilgar.jpeg';
-import chaniImage from '../assets/chani.jpeg';
 
 const characters = [
   { name: 'Paul Atreides', image: paulImage, skills: { stealth: 1, diplomacy: 1 } },
+  { name: 'Chani', image: chaniImage, skills: { stealth: 3, speed: 3, diplomacy: -3 } },
+  { name: 'Stilgar', image: stilgarImage, skills: { stealth: 1, speed: 1, diplomacy: 1, luck: -2 } },
   { name: 'Lady Jessica', image: jessicaImage, skills: { stealth: 2, diplomacy: 2, luck: -2 } },
   { name: 'Duncan Idaho', image: duncanImage, skills: { speed: 3, intelligence: -1 } },
   { name: 'Gurney Halleck', image: gurneyImage, skills: { luck: 2, intelligence: 2, diplomacy: -3 } },
-  { name: 'Stilgar', image: stilgarImage, skills: { stealth: 1, speed: 1, diplomacy: 1, luck: -2 } },
-  { name: 'Chani', image: chaniImage, skills: { stealth: 3, speed: 3, diplomacy: -3 } },
 ];
 
 function CharacterSelection({ onStageChange }) {
@@ -57,7 +57,7 @@ function CharacterSelection({ onStageChange }) {
             }`}
             onClick={() => handleCharacterSelect(character)}
           >
-            <img src={character.image} alt={character.name} />
+            <img src={character.image} alt={character.name} className={styles.characterImage} />
             <h3>{character.name}</h3>
             <ul>
               {Object.entries(character.skills).map(([skill, value]) => (
@@ -69,7 +69,7 @@ function CharacterSelection({ onStageChange }) {
           </div>
         ))}
       </div>
-      <button onClick={handleStartGame} disabled={!selectedCharacter}>
+      <button onClick={handleStartGame} disabled={!selectedCharacter} className={styles.startGameBtn}>
         Start Game
       </button>
     </div>
